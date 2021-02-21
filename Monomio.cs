afab80771eacab21fc7cbd2091fccfb59234b7bd
José Luis Barrera Canto <jose.luis.barrera.canto@gmail.com>
2021-02-20 23:27:39 -0600

    Add project files.

---
+++ Monomio.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Monomios
+{
+    class Monomio
+    {
+        // Variables internas
+        private double coeficiente;
+        private int exponente;
+
+        // Constructores
+        public Monomio()
+        {
+            this.coeficiente = 0;
+            this.exponente = 0;
+        }
+        public Monomio(double coeficiente, int exponente)
+        {
+            // Validamos que el exponente no sea
+            // negativo; de serlo, generamos un
+            // error.
+            if (exponente >= 0)
+            {
+                this.coeficiente = coeficiente;
+                this.exponente = exponente;
+            }
+            else
+            {
+                throw new ArgumentException("El exponente debe ser un entero no negativo.");
+            }
+        }
+
+        // Propiedades (solo lectura)
+        public double Coeficiente
+        {
+            get
+            {
+                return this.coeficiente;
+            }
+        }
+        public int Exponente
+        {
+            get
+            {
+                return this.exponente;
+            }
+        }
+
+        // Métodos
+        public Monomio Sumar(Monomio monomio)
+        {
+            // Para sumar monomios, los exponentes deben
+            // ser iguales.
+            if (this.exponente == monomio.exponente)
+            {
+                return new Monomio(this.coeficiente + monomio.coeficiente, this.exponente);
+            }
+            else
+            {
+                throw new Exception("SUMAR: Operación inválida.");
+            }
+        }
+        public Monomio Restar(Monomio monomio)
+        {
+            // Para restar monomios, los exponentes deben
+            // ser iguales.
+            if (this.exponente == monomio.exponente)
+            {
+                return new Monomio(this.coeficiente - monomio.coeficiente, this.exponente);
+            }
+            else
+            {
+                throw new Exception("SUMAR: Operación inválida.");
+            }
+        }
+        public Monomio Multiplicar(Monomio monomio)
+        {
+             return new Monomio(this.coeficiente * monomio.coeficiente, this.exponente + monomio.exponente);
+        }
+        public Monomio Dividir(Monomio divisor)
+        {
+            // Para dividir monomios, el coeficiente del divisor debe ser diferente
+            // de cero y el exponente del dividendo debe ser mayor o igual al exponente
+            // del divisor.
+            if (divisor.coeficiente != 0 && this.exponente >= divisor.exponente)
+            {
+                return new Monomio(this.coeficiente / divisor.coeficiente, this.exponente - divisor.exponente);
+            }
+            else
+            {
+                throw new Exception("SUMAR: Operación inválida.");
+            }
+        }
+        public double Evaluar(double valor)
+        {
+            return this.coeficiente * Math.Pow(valor, this.exponente);
+        }
+        public string Imprimir()
+        {
+            string resultado, signo, coeficiente, potencia;
+
+            // El signo imprimible es "+" o "-" dependiendo
+            // si el coeficiente es positivo o negativo.
+            signo = this.coeficiente >= 0 ? "+ " : "- ";
+
+            // El coeficiente imprimible es el valor absoluto
+            // del coeficiente,
+            coeficiente = Math.Abs(this.coeficiente).ToString();
+
+            // Si el exponente es 0, la variable vale 1;
+            // si el exponente es 1, solo se imprime la variable;
+            // si el exponente es mayor que 1, se imprime la variable
+            // y el exponente con el símbolo "^".
+            if (this.exponente == 0)
+            {
+                potencia = "";
+            }
+            else if(this.exponente == 1)
+            {
+                potencia = " x";
+            }
+            else
+            {
+                potencia = " x^" + this.exponente;
+            }
+
+            // CASO ESPECIAL: El coeficiente es 0 por lo tanto
+            // todo el monomio vale cero.
+            if (this.coeficiente == 0)
+            {
+                resultado = "+ 0";
+            }
+            else
+            {
+                resultado = signo + coeficiente + potencia;
+                // Verificamos el caso especial de 1x que se 
+                // escribe sin el 1
+                resultado = resultado.Replace("1 x", "x");
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Monomios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProbarImpresionMonomios();
+
+            Console.WriteLine("Este programa te permite realizar operaciones aritméticas");
+            Console.WriteLine("básicas con 2 monomios, siempre que se cumplan las condiciones");
+            Console.WriteLine("para estas operaciones.");
+            Console.WriteLine("");
+
+            Console.WriteLine("*** MONOMIO 1 ***");
+            Console.Write("Dime el coeficiente 1: ");
+            double c1 = Double.Parse(Console.ReadLine());
+            Console.Write("Dime el exponente 1: ");
+            int e1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Monomio M1 = new Monomio(c1, e1);
+
+            Console.WriteLine("*** MONOMIO 2 ***");
+            Console.Write("Dime el coeficiente 2: ");
+            double c2 = Double.Parse(Console.ReadLine());
+            Console.Write("Dime el exponente 2: ");
+            int e2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Monomio M2 = new Monomio(c2, e2);
+
+            Monomio suma = null;
+            Monomio resta = null;
+            Monomio producto = null;
+            Monomio cociente = null;
+
+            // Suma de monomios: M1 + M2
+            try
+            {
+                suma = M1.Sumar(M2);
+                Console.WriteLine("SUMA: " + suma.Imprimir());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("SUMA: Operación inválida.");
+            }
+            // Resta de monomios: M1 - M2
+            try
+            {
+                resta = M1.Restar(M2);
+                Console.WriteLine("RESTA: " + resta.Imprimir());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("RESTA: Operación inválida.");
+            }
+            // Multiplicación de monomios: M1 * M2
+            producto = M1.Multiplicar(M2);
+            Console.WriteLine("PRODUCTO: " + producto.Imprimir());
+            // División de monomios: M1 / M2;
+            try
+            {
+                cociente = M1.Dividir(M2);
+                Console.WriteLine("COCIENTE: " + cociente.Imprimir());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("COCIENTE: Operación inválida.");
+            }
+            Console.WriteLine();
+
+            Console.Write("Dime el valor para evaluar: ");
+            double valor = Double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("El valor del M1 es " + M1.Evaluar(valor));
+            Console.WriteLine("El valor del M2 es " + M2.Evaluar(valor));
+            if (suma != null)
+            {
+                Console.WriteLine("El valor de M1 + M2 es " + suma.Evaluar(valor));
+            }
+            else
+            {
+                Console.WriteLine("El valor de M1 + M2 es operación inválida.");
+            }
+            if (resta != null)
+            {
+                Console.WriteLine("El valor de M1 - M2 es " + resta.Evaluar(valor));
+            }
+            else
+            {
+                Console.WriteLine("El valor de M1 - M2 es operación inválida.");
+            }
+            Console.WriteLine("El valor de M1 * M2 es " + producto.Evaluar(valor));
+            if (cociente != null)
+            {
+                Console.WriteLine("El valor de M1 / M2 es " + cociente.Evaluar(valor));
+            }
+            else
+            {
+                Console.WriteLine("El valor de M1 / M2 es operación inválida.");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("¡MUCHAS GRACIAS POR USAR ESTE PROGRAMA!");
+
+            Console.ReadLine();
+        }
+        static void ProbarImpresionMonomios()
+        {
+            Console.WriteLine(new Monomio(0, 0).Imprimir());
+            Console.WriteLine(new Monomio(0, 1).Imprimir());
+            Console.WriteLine(new Monomio(0, 5).Imprimir());
+            Console.WriteLine(new Monomio(1, 0).Imprimir());
+            Console.WriteLine(new Monomio(1, 1).Imprimir());
+            Console.WriteLine(new Monomio(1, 5).Imprimir());
+            Console.WriteLine(new Monomio(7.5, 0).Imprimir());
+            Console.WriteLine(new Monomio(7.5, 1).Imprimir());
+            Console.WriteLine(new Monomio(7.5, 5).Imprimir());
+            Console.WriteLine(new Monomio(-0, 0).Imprimir());
+            Console.WriteLine(new Monomio(-0, 1).Imprimir());
+            Console.WriteLine(new Monomio(-0, 5).Imprimir());
+            Console.WriteLine(new Monomio(-1, 0).Imprimir());
+            Console.WriteLine(new Monomio(-1, 1).Imprimir());
+            Console.WriteLine(new Monomio(-1, 5).Imprimir());
+            Console.WriteLine(new Monomio(-7.5, 0).Imprimir());
+            Console.WriteLine(new Monomio(-7.5, 1).Imprimir());
+            Console.WriteLine(new Monomio(-7.5, 5).Imprimir());
+        }
+    }
+}
